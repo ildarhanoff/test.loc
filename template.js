4162511c77a535cedf71dc4d2c231ca99c121a27
ildarhanoff <niyaz_ildarhanov@mail.ru>
2025-02-22 21:08:17 +0300

    тест

--- template.js
+++ template.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('menu-hamburger-button').addEventListener('click', function() {
 var texts = document.querySelectorAll('.menu-chapter-text-js');
 texts.forEach(function(text) {
@@ -25,31 +24,4 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
-=======
-document.getElementById('menu-hamburger-button').addEventListener('click', function() {
-var texts = document.querySelectorAll('.menu-chapter-text-js');
-texts.forEach(function(text) {
-    if (text.style.display === "none") {
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
-    });
-});
 
-
-$('.owl-carousel').owlCarousel({
-    margin: 10,            
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 4
-        },
-        1000: {
-            items: 4
-        }
-    }
-});
->>>>>>> 85812b969a39f122043aa444c2620205d9d1d25e
